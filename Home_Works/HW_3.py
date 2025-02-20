ed2e4ec2f9f9d19e21173ba069289a27cd7ec404
masurovaa <masurova09@gmail.com>
2025-02-20 22:56:41 +0600

    LS and HW

---
+++ Home_Works/HW_3.py
@@ -0,0 +1,10 @@
+# Скачать внешний модуль или библиотеку.
+
+# pip install numpy - Installing collected packages: numpy
+# Successfully installed numpy-2.2.3
+
+
+# NumPy (Numerical Python) – это библиотека для работы с многомерными массивами
+# и выполнения математических операций. Она является основой для научных
+# вычислений в Python и используется во многих других библиотеках, включая pandas,
+# scikit-learn, tensorflow и другие.
